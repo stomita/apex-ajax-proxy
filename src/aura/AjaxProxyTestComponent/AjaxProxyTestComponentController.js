a5e00b052b09734429d6878fdae2f205ec63d2ac
Shinichi Tomita <shinichi.tomita@gmail.com>
2017-06-22 13:45:05 +0900

    add visual feedback of http call response in vf page/aura component

--- src/aura/AjaxProxyTestComponent/AjaxProxyTestComponentController.js
+++ src/aura/AjaxProxyTestComponent/AjaxProxyTestComponentController.js
@@ -1,11 +1,14 @@
 ({
 	init: function(cmp, event, helper) {
 		var proxy = helper.getAjaxProxy(cmp);
+		// It is required to define GoogleApi named credential
+		// to connect google api (https://www.googleapis.com/)
 		proxy.request({
 			method: 'GET',
 			url: 'callout:GoogleApi/oauth2/v3/userinfo'
 		}).then(function(res) {
 			console.log(res.statusCode, res.body);
+			cmp.set('v.email', res.body.email);
 		}).catch(function(err) {
 			console.error(err);
 		});
